c0e1683b88ab22fcdd4cfa05f9c13b6ff78fbef4
James Reeves <jreeves@weavejester.com>
2017-02-10 17:41:47 +0000

    Add Integrant methods for Timbre

--- src/duct/logger/timbre.clj
+++ src/duct/logger/timbre.clj
@@ -1,6 +1,21 @@
-(ns duct.logger.timbre)
+(ns duct.logger.timbre
+  (:require [duct.core.protocols :as p]
+            [integrant.core :as ig]
+            [taoensso.timbre :as timbre]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmethod ig/init-key ::println [_ options]
+  (timbre/println-appender options))
+
+(defmethod ig/init-key ::spit [_ options]
+  (timbre/spit-appender options))
+
+(derive :duct.logger/timbre :duct/logger)
+
+(defrecord TimbreLogger [config]
+  p/Logger
+  (log [_ level key]      (timbre/log* config level key))
+  (log [_ level key data] (timbre/log* config level key data))
+  (log-ex [_ level ex]    (timbre/log* config level ex)))
+
+(defmethod ig/init-key :duct.logger/timbre [_ config]
+  (->TimbreLogger config))
