99cc350f0cc52b69d1b5e83c570af700b0031cb4
James Reeves <jreeves@weavejester.com>
2017-04-12 15:58:47 +0100

    Add test for spit appender

--- test/duct/logger/timbre_test.clj
+++ test/duct/logger/timbre_test.clj
@@ -30,3 +30,21 @@
            WARN\ \[duct\.logger\.timbre-test:32\]\ -\ 
            :duct\.logger\.timbre-test/testing\ \{:foo\ \"bar\"\}\n"
          (with-out-str (logger/log logger :warn ::testing {:foo "bar"}))))))
+
+(deftest spit-appender-test
+  (let [tempfile (doto (java.io.File/createTempFile "timbre" "log") (.deleteOnExit))
+        config   {::logger/timbre {:level :info, :appenders {:spit (ig/ref ::timbre/spit)}}
+                  ::timbre/spit   {:fname (str tempfile)}}
+        logger   (::logger/timbre (ig/init config))]
+    (logger/log logger :info ::testing)
+    (logger/log logger :warn ::testing {:foo "bar"})
+    (is (re-matches
+         #"(?x)
+           \d\d-\d\d-\d\d\ \d\d:\d\d:\d\d\ [^\s]+\ 
+           INFO\ \[duct\.logger\.timbre-test:39\]\ -\ 
+           :duct\.logger\.timbre-test/testing\n
+
+           \d\d-\d\d-\d\d\ \d\d:\d\d:\d\d\ [^\s]+\ 
+           WARN\ \[duct\.logger\.timbre-test:40\]\ -\ 
+           :duct\.logger\.timbre-test/testing\ \{:foo\ \"bar\"\}\n"
+         (slurp tempfile)))))
