40b39d77d245c514b424b414a8a6ed7453d0ffdd
James Reeves <jreeves@weavejester.com>
2017-04-12 15:50:28 +0100

    Add test for println appender

--- test/duct/logger/timbre_test.clj
+++ test/duct/logger/timbre_test.clj
@@ -13,5 +13,20 @@
         logger (::logger/timbre (ig/init config))]
     (is (= (with-out-str (logger/log logger :info ::testing))
            ":duct.logger.timbre-test/testing\n"))
-    (is (= (with-out-str (logger/log logger :info ::testing {:foo "bar"}))
+    (is (= (with-out-str (logger/log logger :warn ::testing {:foo "bar"}))
            ":duct.logger.timbre-test/testing {:foo \"bar\"}\n"))))
+
+(deftest println-appender-test
+  (let [config {::logger/timbre  {:level :info, :appenders {:prn (ig/ref ::timbre/println)}}
+                ::timbre/println {}}
+        logger (::logger/timbre (ig/init config))]
+    (is (re-matches
+         #"(?x)\d\d-\d\d-\d\d\ \d\d:\d\d:\d\d\ [^\s]+\ 
+           INFO\ \[duct\.logger\.timbre-test:27\]\ -\ 
+           :duct\.logger\.timbre-test/testing\n"
+         (with-out-str (logger/log logger :info ::testing))))
+    (is (re-matches
+         #"(?x)\d\d-\d\d-\d\d\ \d\d:\d\d:\d\d\ [^\s]+\ 
+           WARN\ \[duct\.logger\.timbre-test:32\]\ -\ 
+           :duct\.logger\.timbre-test/testing\ \{:foo\ \"bar\"\}\n"
+         (with-out-str (logger/log logger :warn ::testing {:foo "bar"}))))))
