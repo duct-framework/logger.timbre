801a36e78ff6c6331bb7edfef3a5791165b70b95
James Reeves <jreeves@weavejester.com>
2017-04-12 15:27:24 +0100

    Add test for brief appender

--- test/duct/logger/timbre_test.clj
+++ test/duct/logger/timbre_test.clj
@@ -1,7 +1,17 @@
 (ns duct.logger.timbre-test
   (:require [clojure.test :refer :all]
             [duct.logger :as logger]
-            duct.logger.timbre))
+            [duct.logger.timbre :as timbre]
+            [integrant.core :as ig]))
 
 (deftest key-test
   (is (isa? :duct.logger/timbre :duct/logger)))
+
+(deftest brief-appender-test
+  (let [config {::logger/timbre {:level :info, :appenders {:brief (ig/ref ::timbre/brief)}}
+                ::timbre/brief  {}}
+        logger (::logger/timbre (ig/init config))]
+    (is (= (with-out-str (logger/log logger :info ::testing))
+           ":duct.logger.timbre-test/testing\n"))
+    (is (= (with-out-str (logger/log logger :info ::testing {:foo "bar"}))
+           ":duct.logger.timbre-test/testing {:foo \"bar\"}\n"))))
