1400c0016b72da4b24aa876bc4771e724179c15e
James Reeves <jreeves@weavejester.com>
2017-04-17 22:37:53 +0100

    Update Timbre to 4.10.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.8.0"]
+                 [com.taoensso/timbre "4.10.0"]
                  [duct/logger "0.1.1"]
                  [integrant "0.3.3"]])
