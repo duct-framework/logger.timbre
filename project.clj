a0c8cfbd4a44c797949beb989f5a4fe33d4a4848
James Reeves <jreeves@weavejester.com>
2017-07-29 22:04:47 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/logger.timbre"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [com.taoensso/timbre "4.10.0"]
                  [duct/logger "0.1.1"]
-                 [integrant "0.3.3"]])
+                 [integrant "0.5.0"]])
